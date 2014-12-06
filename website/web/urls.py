f1b533b76d7312bc571cc66d3c5d59f74ab295b9
ssanupam24 <ssanupam24@gmail.com>
2014-12-06 00:41:39 -0500

    added graphlab recco

--- website/web/urls.py
+++ website/web/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 from . import views
 urlpatterns = patterns('',url(r'^$', views.get_name),
-        url(r'^reco/(?P<userId>\S+)$', views.show_reco)
+        url(r'^reco/(?P<userId>\S+)/(?P<algorithm>\S+)$', views.show_reco)
         )

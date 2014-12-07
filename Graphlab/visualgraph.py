604fd64b0abcfeb36905e8af326459006ead6743
ssanupam24 <ssanupam24@gmail.com>
2014-12-06 20:08:21 -0500

    updated city and state

--- Graphlab/visualgraph.py
+++ Graphlab/visualgraph.py
@@ -2,13 +2,16 @@ from pymongo import MongoClient
 from settings import Settings
 import operator
 import csv
+import sys
+sys.path.insert(0, '/home/anupam/Documents/Data_Science/Data Science/GraphlabRecco/yelp_dataset_challenge_academic_dataset')
 
 usergraph_collection = MongoClient(Settings.MONGO_CONNECTION_STRING)[Settings.TOPICS_DATABASE][Settings.USERGRAPH_COLLECTION]
+business_collection = MongoClient(Settings.MONGO_CONNECTION_STRING)[Settings.REVIEWS_DATABASE][
+    Settings.BUSINESS_COLLECTION]
 
 #print usergraph_collection.count()
 
 user_reco = usergraph_collection.find()
-
 business_dict = {}
 counter = 0
 for user in user_reco:
@@ -21,4 +24,7 @@ print counter
 business_list = sorted(business_dict.items(), key=operator.itemgetter(1), reverse=True)
 writer = csv.writer(open('recoList.csv', 'wb'))
 for key, value in business_list:
-    writer.writerow([key, value])
+    business_info = business_collection.find({"business_id":key})
+    for business in business_info:
+        writer.writerow([key, value, business["city"], business["state"]])
+    business_info.rewind()
